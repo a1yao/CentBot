76bf3feaf9b9814ac533454e26e5ccd667c8c865
unknown <anthony.anthony.yao@gmail.com>
2019-12-29 16:24:08 -0800

    fixed sign issues

--- centtest.py
+++ centtest.py
@@ -7,6 +7,16 @@ histList = []
 f = open("key.txt", "r")
 token = f.readline()
 memList = []
+fixed = ""
+
+def fixSign(val):
+    if val >= 0:
+        fixed = '${:,.2f}'.format(val)
+    if val < 0:
+        fixed = '-${:,.2f}'.format(val * -1)
+    return(fixed)
+
+
 
 @client.event
 async def on_ready():
@@ -16,9 +26,13 @@ async def on_ready():
 
 @client.command()
 async def create (ctx, member : discord.Member, debt = 0.0):
+    if debt >= 0:
+        temp = '${:,.2f}'.format(debt)
+    if debt < 0:
+        temp = '-${:,.2f}'.format(debt*-1)
     createBed = discord.Embed(
         title = member.name,
-        description = '${:,.2f}'.format(debt)
+        description = temp
     )
     await ctx.send(embed = createBed)
     memList.append([member.name, float(debt)])
@@ -34,14 +48,16 @@ async def give (ctx, member : discord.Member, amount, *, reason = None):
             i[1] = i[1] - float(amount)
             # await ctx.send(f'{i[0]}, {i[1]}')
             giver = i[0]
-    formattedAmount = '${:,.2f}'.format(float(amount))
-    print(formattedAmount)
+    if float(amount) >= 0:
+        temp3 = '${:,.2f}'.format(float(amount))
+    if float(amount) < 0:
+        temp3 = '-${:,.2f}'.format(float(amount)*-1)
     if reason == None:
-        await ctx.send(f"{giver} has given ${formattedAmount} to {receiver}")
-        histList.append(f"{giver} has given ${formattedAmount} to {receiver}")
+        await ctx.send(f"{giver} has given {temp3} to {receiver}")
+        histList.append(f"{giver} has given {temp3} to {receiver}")
     else:
-        await ctx.send(f"{giver} has given ${formattedAmount} to {receiver} for {reason}")
-        histList.append(f"{giver} has given ${formattedAmount} to {receiver} for {reason}")
+        await ctx.send(f"{giver} has given {temp3} to {receiver} for {reason}")
+        histList.append(f"{giver} has given {temp3} to {receiver} for {reason}")
 
 @client.command()
 async def dash (ctx):
@@ -49,7 +65,11 @@ async def dash (ctx):
         title = "Dashboard"
     )
     for i in memList:
-        membed.add_field(name = i[0], value = '${:,.2f}'.format(i[1]), inline= False)
+        if i[1] >= 0:
+            temp2 = '${:,.2f}'.format(i[1])
+        if i[1] < 0:
+            temp2 = '-${:,.2f}'.format(i[1]*-1)
+        membed.add_field(name = i[0], value = temp2, inline= False)
     await ctx.send(embed = membed)
 
 @client.command()
